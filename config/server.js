a3cbf6d20b6b1999f4266a39701c82d327051e97
KIM-JUNHO <prpr48@naver.com>
2021-06-23 22:21:22 +0900

    yarn create strapi-app shopping

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e102d2f345cb927c7062bf13ef8e796'),
+    },
+  },
+});
